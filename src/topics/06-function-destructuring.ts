ef5909dafec2d892046cc42037f374722ce92047
JairoAgustinMendozaArreola <jairoa.mendozaa@gmail.com>
2023-08-14 20:31:29 -0600

    21. Resolución de la tarea - Desestructuración

--- src/topics/06-function-destructuring.ts
+++ src/topics/06-function-destructuring.ts
@@ -19,14 +19,19 @@ interface TaxCalculationOptions {
     products: Product[];
 }
 
-function taxCalculation( options: TaxCalculationOptions ): number[] {
+//function taxCalculation( options: TaxCalculationOptions ): [number, number] {
+//function taxCalculation( {tax, products}: TaxCalculationOptions ): [number, number] {
+function taxCalculation( options: TaxCalculationOptions ): [number, number] {
+    
+    const { tax, products } = options;
+    
     let total = 0;
 
-    options.products.forEach( product => {
-        total += product.price; 
+    products.forEach( ({ price }) => {
+        total += price; 
     });
 
-    return [total, total * options.tax ];
+    return [total, total * tax ];
 } 
 
 //Se podría renombrar como products para reducir el codigo en const result
@@ -34,14 +39,14 @@ const shoppingCart = [phone, tablet];
 const tax = 0.15;
 
 
-const result = taxCalculation({
+const [ total, taxTotal ] = taxCalculation({
     products: shoppingCart,
     //tax: tax es lo mismo que la line a de abajo por ECMA-script
     tax
 })
 
-console.log('Total', result[0]);
-console.log('Tax', result[1]);
+console.log('Total', total);
+console.log('Tax', taxTotal);
 
 
 
