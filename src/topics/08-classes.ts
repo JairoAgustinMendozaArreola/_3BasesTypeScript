bdaadadae3c4c70e135ec5b72613484d8cda59e5
JairoAgustinMendozaArreola <jairoa.mendozaa@gmail.com>
2023-08-14 23:34:02 -0600

    23. Clases básicas

---
+++ src/topics/08-classes.ts
@@ -0,0 +1,16 @@
+
+
+
+export class Person {
+    public name: string;
+    private address: string;
+
+    constructor() {
+        this.name = 'Fernando';
+        this.address = 'New York';
+    }
+}
+
+const ironman = new Person();
+
+console.log(ironman.address);
