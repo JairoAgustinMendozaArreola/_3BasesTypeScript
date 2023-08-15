51ec07041f99be1a0f954b8e14c24f1f35b4f9a2
JairoAgustinMendozaArreola <jairoa.mendozaa@gmail.com>
2023-08-15 00:23:45 -0600

    25. Extender una clase

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -11,6 +11,17 @@ export class Person {
         ) {}
 }
 
-const ironman = new Person('IronMman');
+export class Hero extends Person{
+    
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+    ){
+        super(realName, 'New York' );
+    }
+}
+
+const ironman = new Hero('IronMman',45,'Tony');
 
 console.log(ironman);
