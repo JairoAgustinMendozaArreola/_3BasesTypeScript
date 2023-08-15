ec5eb25421f4d7098d213621729eeb43b2a49d79
JairoAgustinMendozaArreola <jairoa.mendozaa@gmail.com>
2023-08-15 13:58:12 -0600

    26. Priorizar composición sobre herencia

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -1,3 +1,4 @@
+import { Person } from './08-classes';
 
 
 
@@ -6,22 +7,39 @@ export class Person {
     // private address: string;
 
     constructor(
-        public name: string,
+        public firstName: string,
+        public lastName: string,
         private address: string = 'No Address'
         ) {}
 }
 
-export class Hero extends Person{
+// export class Hero extends Person{
     
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string,
+//     ){
+//         super(realName, 'New York' );
+//     }
+// 
+
+export class Hero{
+    
+    //public person: Person;
+
     constructor(
         public alterEgo: string,
         public age: number,
         public realName: string,
+        public person: Person,
     ){
-        super(realName, 'New York' );
+        //this.person = new Person(realName);
     }
 }
 
-const ironman = new Hero('IronMman',45,'Tony');
+const tony = new Person('Tony', 'Stark','New York');
+
+const ironman = new Hero('IronMman',45,'Tony',tony);
 
 console.log(ironman);
